81d35ef093af2a39c574cd7c17520fe435eddea2
kinko0317 <1983636861@qq.com>
2025-07-08 22:28:37 +0800

    On branch kinko

--- backend/news_backend/app/__init__.py
+++ backend/news_backend/app/__init__.py
@@ -14,7 +14,7 @@ CORS(app)
 # print("MAIL_PASSWORD from env:", os.environ.get("MAIL_PASSWORD"))
 
 # 配置数据库连接
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:19232416@127.0.0.1:3306/device_monitoring'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Ly85190316,@127.0.0.1:3306/device_monitoring'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 这个不关会有警告！
 db = SQLAlchemy(app)
 
@@ -23,3 +23,6 @@ app.register_blueprint(auth, url_prefix='/auth')
 
 from app.api.Data_return import Datareturn_bp
 app.register_blueprint(Datareturn_bp, url_prefix='/device')
+
+from app.api.StripControl import control_bp
+app.register_blueprint(control_bp, url_prefix='/command')
--- backend/news_backend/app/api/StripControl.py
+++ backend/news_backend/app/api/StripControl.py
@@ -2,13 +2,7 @@ from flask import request, Blueprint, jsonify
 from app.models.Process_Data import get_latest_electrical_params
 from app.utils.common import api_response
 
-from huaweicloudsdkcore.exceptions import exceptions
-from huaweicloudsdkcore.region.region import Region
-from huaweicloudsdkiotda.v5 import *
-from huaweicloudsdkcore.auth.credentials import BasicCredentials
-from huaweicloudsdkcore.auth.credentials import DerivedCredentials
-
-control_bp = Blueprint('StripControl', __name__)
+control_bp = Blueprint('command', __name__)
 
 @control_bp.route('/', methods=['POST'])
 def Control_Strip():
